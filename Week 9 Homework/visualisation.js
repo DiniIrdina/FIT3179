702e980b7c0ce432c675b98d2a04af99ce95990b
dahm0004 <dahm0004@student.monash.edu>
2022-10-09 17:11:01 +0800

    tried different script

--- Week 9 Homework/visualisation.js
+++ Week 9 Homework/visualisation.js
@@ -1,5 +1,5 @@
 var vg_1 = "chloropleth_map.vg.json";
-vegaEmbed("#vis1", vg_1).then(function(result) {
+vegaEmbed("#map_vis", vg_1).then(function(result) {
 
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
